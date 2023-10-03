445274186630514f332504032e7b975831fb7a53
connectuerstek <144154507+connectuerstek@users.noreply.github.com>
2023-10-03 12:55:38 -0400

    Update pipelineBuild.groovy

--- vars/pipelineBuild.groovy
+++ vars/pipelineBuild.groovy
@@ -1,5 +1,4 @@
 def call(body) {
-    List<String> artifactsList = []
     def pipelineParams = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
@@ -11,18 +10,49 @@ def call(body) {
         stages {
             stage('Build Application') {
                 steps {
-                    // // mvn command but use inputs
-                    // sh "mvn ${pipelineParams.mavengoals}"
-                  script {
-                    if (pipelineParams.buildtype == 'frontend'){
-                      bat "cd && zip -r ${pipelineParams.zipFileName} ."
-                      }
-                    else if (pipelineParams.buildtype == 'backend'){
-                      bat ""C:\\Program Files\\apache-maven-3.9.4\\bin\\mvn" ${pipelineParams.mavengoals}"
+                    script {
+                        if (pipelineParams.buildtype == 'frontend') {
+                            // Create a zip file in the current directory
+                            bat "cd frontend && zip -r ${pipelineParams.zipFileName} ."
+                        } else if (pipelineParams.buildtype == 'backend') {
+                            // Use the Maven executable with the provided goals
+                            bat "\"C:\\Program Files\\apache-maven-3.9.4\\bin\\mvn\" ${pipelineParams.mavengoals}"
+                        } else {
+                            error 'Invalid buildtype provided. Use "frontend" or "backend".'
+                        }
                     }
-                  }
-					
                 }
+	      stage('Publish to Aritifactory') {
+		      script {
+			      def serverId = 'artifactory'
+			      if (pipelineParams.buildtype == 'frontend') {
+				      def targetPath = "npm-local/com/ui/test/${pipelineParams.zipFileName}"
+				      rtUpload (
+					      serverId: serverId,
+					      spec: """{
+	                                          "files": [
+					              {
+		                                         "pattern": "${pipelineParams.zipFileName}",
+                                                         "target": "${targetPath}"
+							 }
+	                                           ]
+					      }"""
+				      )
+			      }
+			      else if (pipelineParams.buildtype == 'backend') {
+				      def server = Artifactory.server(serverId)
+				      def rtMaven =Artifactory.newMavenBuild()
+
+				      rtMaven.tool = 'Maven'
+				      rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: server
+
+				      def buildInfo = rtMaven.run pom: 'pom.xml', goals: '${pipelineParams.mavengoals}'
+				      server.publishBuildInfo buildInfo
+				      
+			      }
+		      }
+		      
+	      }
             }
         }
     }
